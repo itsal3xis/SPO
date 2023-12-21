fb0ea01fd6436939359c0b6d708f3a43619182aa
itsal3xis <alexqc2006@icloud.com>
2023-12-21 14:46:18 -0500

    Changes

--- terminal.py
+++ terminal.py
@@ -2,3 +2,29 @@ def spo_terminal():
     print("-" * 14)
     print("| SPO TERMINAL |")
     print("-" * 14)
+
+    while True:
+        command = input(">")
+
+        command = command.lower()
+
+        if command == "?" or command == "help" or command == "?help":
+            print("Here is the help terminal ... If you have any questions about a command just put (?) before a command, for example ?help")
+            #print("")
+
+        elif command == "spo" or command == "?spo":
+            print("SPO, for Simplified Python Os is a command line open source os entirely in python")
+            print("You can follow the project on Github")
+            print("https://github.com/itsal3xis/SPO")
+
+        elif command == "quit":
+            return
+        
+        elif command == "?quit":
+            print("This command return back to the main menu")
+        
+        
+        else:
+            print("Command unknown... try again or help / ?")
+
+        print("\n")
